ec458069162bf0a7e067f2931ca811fc652e8d4c
gjersing <csgjersing@gmail.com>
2022-02-23 15:34:47 -0800

    Implement random text color on button click

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const buttons = document.querySelectorAll("button");
 const crash = new Audio('sounds/crash.mp3');
 
-document.querySelector(".w").addEventListener("click", function () {
-  crash.play();
-});
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function() {
+    this.style.color = "#" + Math.floor(Math.random()*16777215).toString(16);
+    switch (this.innerHTML) {
+      case 'w':
+        crash.play();
+        break;
+      default:
+
+    }
+  });
+}
